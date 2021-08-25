439a84ed3c044f49abedc5a5ddcee64612b98577
Vinicius <viniciusdesouto@gmail.com>
2021-08-24 23:26:47 -0300

    validade if CPF already exist

--- src/index.js
+++ src/index.js
@@ -10,12 +10,18 @@ app.use(express.json());
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  const id = uuidv4()
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  )
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" })
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   })
 
